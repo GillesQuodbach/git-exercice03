7b50b2b8a239963e41eb903d1f09a9d10825a07e
Gilles <quodbach.gilles@gmail.com>
2024-01-04 14:46:44 +0100

    exercice terminé, reste gestion des exceptions

--- src/gitExercice03/GitExo03.java
+++ src/gitExercice03/GitExo03.java
@@ -109,20 +109,11 @@ public class GitExo03 {
 		Scanner scan = new Scanner(System.in);
 
 		boolean replay = true;
-		// Liste des chevaux
+		// Liste des chevaux au départ
 		HashMap<Integer, Integer[]> horsesListHashMap = new HashMap<>();
 
-		// Variables
-		int raceDistance = 2400;
-		int gameChoice = 0;
-
-		int horseSpeedValueIndex = 0;
-		int horseDistanceValueIndex = 1;
-		int horseTimeValueIndex = 2;
-
-		ArrayList<Integer> tierceWinnerArrayList = new ArrayList<>();
-		ArrayList<Integer> quarteWinnerArrayList = new ArrayList<>();
-		ArrayList<Integer> quinteWinnerArrayList = new ArrayList<>();
+		// Liste des vainqueurs
+		Set<Integer> winnerSetList = new HashSet<>();
 
 		// Mise en place des règles du jeu
 		// Choix du nombre de chevaux au départ
@@ -139,24 +130,44 @@ public class GitExo03 {
 		for (int i = 0; i < horsesOnStart; i++) {
 			horsesListHashMap.put(i, new Integer[] { 0, 0 });
 		}
-		
-		
-		while (replay) {
+
+		System.out.println("Voulez-vous jouer au tiercé (1), au quarté (2) ou au quinté (3) ?");
+		int userGameResponse = Integer.parseInt(scan.nextLine());
+		while (userGameResponse < 1 || userGameResponse > 3) {
+
+			System.out.println("Veuillez entrer 1 ou 2 ou 3 !");
+			userGameResponse = Integer.parseInt(scan.nextLine());
+		}
+
+		int winnerSetListSize = 0;
+
+		switch (userGameResponse) {
+		case 1:
+			winnerSetListSize = 3;
+			break;
+		case 2:
+			winnerSetListSize = 4;
+			break;
+		case 3:
+			winnerSetListSize = 5;
+		}
+
+		while (replay && (winnerSetListSize > winnerSetList.size() )) {
 			int counter = 1;
 			;
 			for (int i = 0; i < horsesOnStart; i++) {
 				// On récupère le cheval concerné
-				System.out.println("Nombre de boucles = " + counter++);
+//				System.out.println("Nombre de boucles = " + counter++);
 				Integer[] values = horsesListHashMap.get(i);
-				System.out.println("***values***" + Arrays.toString(values));
-				
+				System.out.println("Infos cheval " + (i + 1) + " " + Arrays.toString(values));
+
 				// jet du dé (OK)
 				int randomDieRoll = dieRoll();
-				System.out.println("Valeur du lancé du dé: " + randomDieRoll);
+//				System.out.println("Valeur du lancé du dé: " + randomDieRoll);
 
 				// Calcul du delta à appliquer
 				int lapDelta = updateSpeedCalc(randomDieRoll, values[0]);
-				System.out.println("Valeur du delta vitesse:" + lapDelta);
+//				System.out.println("Valeur du delta vitesse:" + lapDelta);
 
 				values[0] += lapDelta;
 				if (values[0] <= 0) {
@@ -164,13 +175,18 @@ public class GitExo03 {
 				} else if (values[0] >= 6) {
 					values[0] = 6;
 				}
-				
+
 				// Calcul de la distance avec vitesse actuelle
 				values[1] += distanceCalc(values[0]);
-				
-				horsesListHashMap.put(i,values);
-			
+
+				horsesListHashMap.put(i, values);
+
+				if ((values[1] >= 2400) && (winnerSetList.size() < winnerSetListSize)) {
+					winnerSetList.add(i);
+				}
+
 			}
+			
 			System.out.println("Relancer le dé ? Y/N");
 			String playerDieRoll = scan.nextLine().toLowerCase();
 
@@ -180,7 +196,8 @@ public class GitExo03 {
 				replay = false;
 			}
 		}
+		System.out.println("La courses est terminée !");
+		System.out.println("Liste d'arrivée: " + (winnerSetList));
 
 	}
-
 }
